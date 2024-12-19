6426afa7ef19b15a91eb97233eb97c6dd252057b
stackgen-automation <automation@stackgen.com>
2024-12-19 04:28:58 +0000

    Automated commit - Test commit to Main

---
+++ terraform/outputs.tf
@@ -0,0 +1,47 @@
+
+output "aws_lambda_stackgen_150a57_function_function_arn" {
+  value = module.stackgen_706c35f7-02dc-5ac3-856e-300383f44a52.function_arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_150a57_function_function_name" {
+  value = module.stackgen_706c35f7-02dc-5ac3-856e-300383f44a52.function_name
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_150a57_function_function_url" {
+  value = module.stackgen_706c35f7-02dc-5ac3-856e-300383f44a52.function_url
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_150a57_function_invoke_arn" {
+  value = module.stackgen_706c35f7-02dc-5ac3-856e-300383f44a52.invoke_arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_150a57_function_arn" {
+  value = module.stackgen_706c35f7-02dc-5ac3-856e-300383f44a52.arn
+  sensitive = false
+}
+
+
+output "aws_iam_role_stackgen_150a57_role_arn" {
+  value = module.stackgen_db72f711-9d46-5827-97e6-747603fcd881.arn
+  sensitive = false
+}
+
+output "aws_iam_role_stackgen_150a57_role_name" {
+  value = module.stackgen_db72f711-9d46-5827-97e6-747603fcd881.name
+  sensitive = false
+}
+
+
+output "aws_cloudwatch_log_group_aws_lambda_stackgen_150a57_function_arn" {
+  value = module.stackgen_4d761851-7422-5e4e-840c-39aaf875fa9b.arn
+  sensitive = false
+}
+
+output "aws_cloudwatch_log_group_aws_lambda_stackgen_150a57_function_name" {
+  value = module.stackgen_4d761851-7422-5e4e-840c-39aaf875fa9b.name
+  sensitive = false
+}
