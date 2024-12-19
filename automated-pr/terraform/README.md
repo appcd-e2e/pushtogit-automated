eba95109c3e0b506871990ee79a37e468da961f0
stackgen-automation <automation@stackgen.com>
2024-12-19 04:53:42 +0000

    Automated commit - Test After Merge

--- automated-pr/terraform/README.md
+++ automated-pr/terraform/README.md
@@ -1,3 +1,3 @@
 # README
 This is a readme file for IaC generated with StackGen.
-You can modify your appStack -> [here](http://main.dev.stackgen.com/appstacks/86bb7d54-b169-409c-a31b-2fc2af69ed0e)
+You can modify your appStack -> [here](http://stage.dev.stackgen.com/appstacks/b493145f-1c24-49d5-951c-afb566c48469)
--- automated-pr/terraform/outputs.tf
+++ automated-pr/terraform/outputs.tf
@@ -1,47 +1,47 @@
 
-output "aws_lambda_stackgen_5533b1_function_invoke_arn" {
-  value = module.stackgen_d4af4d11-1e34-56a1-94fb-539614dc7f20.invoke_arn
+output "aws_lambda_stackgen_70787d_function_function_arn" {
+  value = module.stackgen_67592397-6dd4-595e-8ec8-6bb257b072d5.function_arn
   sensitive = false
 }
 
-output "aws_lambda_stackgen_5533b1_function_arn" {
-  value = module.stackgen_d4af4d11-1e34-56a1-94fb-539614dc7f20.arn
+output "aws_lambda_stackgen_70787d_function_function_name" {
+  value = module.stackgen_67592397-6dd4-595e-8ec8-6bb257b072d5.function_name
   sensitive = false
 }
 
-output "aws_lambda_stackgen_5533b1_function_function_arn" {
-  value = module.stackgen_d4af4d11-1e34-56a1-94fb-539614dc7f20.function_arn
+output "aws_lambda_stackgen_70787d_function_function_url" {
+  value = module.stackgen_67592397-6dd4-595e-8ec8-6bb257b072d5.function_url
   sensitive = false
 }
 
-output "aws_lambda_stackgen_5533b1_function_function_name" {
-  value = module.stackgen_d4af4d11-1e34-56a1-94fb-539614dc7f20.function_name
+output "aws_lambda_stackgen_70787d_function_invoke_arn" {
+  value = module.stackgen_67592397-6dd4-595e-8ec8-6bb257b072d5.invoke_arn
   sensitive = false
 }
 
-output "aws_lambda_stackgen_5533b1_function_function_url" {
-  value = module.stackgen_d4af4d11-1e34-56a1-94fb-539614dc7f20.function_url
-  sensitive = false
-}
-
-
-output "aws_iam_role_stackgen_5533b1_role_arn" {
-  value = module.stackgen_66019561-1e41-55ee-8656-f0d15cedf98b.arn
-  sensitive = false
-}
-
-output "aws_iam_role_stackgen_5533b1_role_name" {
-  value = module.stackgen_66019561-1e41-55ee-8656-f0d15cedf98b.name
+output "aws_lambda_stackgen_70787d_function_arn" {
+  value = module.stackgen_67592397-6dd4-595e-8ec8-6bb257b072d5.arn
   sensitive = false
 }
 
 
-output "aws_cloudwatch_log_group_aws_lambda_stackgen_5533b1_function_arn" {
-  value = module.stackgen_6c7ca2dc-a985-5e36-9aa2-0e26a0cd003c.arn
+output "aws_cloudwatch_log_group_aws_lambda_stackgen_70787d_function_arn" {
+  value = module.stackgen_3a80d771-7e4d-508a-826c-ded2f1c1bec6.arn
   sensitive = false
 }
 
-output "aws_cloudwatch_log_group_aws_lambda_stackgen_5533b1_function_name" {
-  value = module.stackgen_6c7ca2dc-a985-5e36-9aa2-0e26a0cd003c.name
+output "aws_cloudwatch_log_group_aws_lambda_stackgen_70787d_function_name" {
+  value = module.stackgen_3a80d771-7e4d-508a-826c-ded2f1c1bec6.name
+  sensitive = false
+}
+
+
+output "aws_iam_role_stackgen_70787d_role_arn" {
+  value = module.stackgen_fa26481c-061a-5e7a-88c5-0e9498a8b821.arn
+  sensitive = false
+}
+
+output "aws_iam_role_stackgen_70787d_role_name" {
+  value = module.stackgen_fa26481c-061a-5e7a-88c5-0e9498a8b821.name
   sensitive = false
 }
