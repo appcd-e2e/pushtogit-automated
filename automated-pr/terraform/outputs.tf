4b5a5d955ddb93c84e77edc84423124be359385e
stackgen-automation <automation@stackgen.com>
2024-12-19 05:19:16 +0000

    Automated commit - Test 1 Check

--- automated-pr/terraform/outputs.tf
+++ automated-pr/terraform/outputs.tf
@@ -1,4 +1,30 @@
 
+output "aws_lambda_stackgen_5533b1_function_function_url" {
+  value = module.stackgen_d4af4d11-1e34-56a1-94fb-539614dc7f20.function_url
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_5533b1_function_invoke_arn" {
+  value = module.stackgen_d4af4d11-1e34-56a1-94fb-539614dc7f20.invoke_arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_5533b1_function_arn" {
+  value = module.stackgen_d4af4d11-1e34-56a1-94fb-539614dc7f20.arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_5533b1_function_function_arn" {
+  value = module.stackgen_d4af4d11-1e34-56a1-94fb-539614dc7f20.function_arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_5533b1_function_function_name" {
+  value = module.stackgen_d4af4d11-1e34-56a1-94fb-539614dc7f20.function_name
+  sensitive = false
+}
+
+
 output "aws_iam_role_stackgen_5533b1_role_arn" {
   value = module.stackgen_66019561-1e41-55ee-8656-f0d15cedf98b.arn
   sensitive = false
@@ -17,31 +43,5 @@ output "aws_cloudwatch_log_group_aws_lambda_stackgen_5533b1_function_arn" {
 
 output "aws_cloudwatch_log_group_aws_lambda_stackgen_5533b1_function_name" {
   value = module.stackgen_6c7ca2dc-a985-5e36-9aa2-0e26a0cd003c.name
-  sensitive = false
-}
-
-
-output "aws_lambda_stackgen_5533b1_function_function_arn" {
-  value = module.stackgen_d4af4d11-1e34-56a1-94fb-539614dc7f20.function_arn
-  sensitive = false
-}
-
-output "aws_lambda_stackgen_5533b1_function_function_name" {
-  value = module.stackgen_d4af4d11-1e34-56a1-94fb-539614dc7f20.function_name
-  sensitive = false
-}
-
-output "aws_lambda_stackgen_5533b1_function_function_url" {
-  value = module.stackgen_d4af4d11-1e34-56a1-94fb-539614dc7f20.function_url
-  sensitive = false
-}
-
-output "aws_lambda_stackgen_5533b1_function_invoke_arn" {
-  value = module.stackgen_d4af4d11-1e34-56a1-94fb-539614dc7f20.invoke_arn
-  sensitive = false
-}
-
-output "aws_lambda_stackgen_5533b1_function_arn" {
-  value = module.stackgen_d4af4d11-1e34-56a1-94fb-539614dc7f20.arn
   sensitive = false
 }
