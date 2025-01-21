ebd8f8b3a8afd36c19144b8a3e141b88464a3cea
stackgen-automation <automation@stackgen.com>
2025-01-21 18:00:22 +0000

    Automated Commit - 1737482380892

---
+++ terraform/README.md
@@ -0,0 +1,3 @@
+# README
+This is a readme file for IaC generated with StackGen.
+You can modify your appStack -> [here](http://main.dev.stackgen.com/appstacks/8a142c0b-52d4-4fb7-8627-e9e7f7a01646)
---
+++ terraform/outputs.tf
@@ -0,0 +1,47 @@
+
+output "aws_iam_role_stackgen_bb25b8_role_arn" {
+  value = module.stackgen_56c656de-b92f-504b-8885-93f760ca7da0.arn
+  sensitive = false
+}
+
+output "aws_iam_role_stackgen_bb25b8_role_name" {
+  value = module.stackgen_56c656de-b92f-504b-8885-93f760ca7da0.name
+  sensitive = false
+}
+
+
+output "aws_cloudwatch_log_group_aws_lambda_stackgen_bb25b8_function_arn" {
+  value = module.stackgen_f302a517-752b-52d7-b0d6-359ca150c8c8.arn
+  sensitive = false
+}
+
+output "aws_cloudwatch_log_group_aws_lambda_stackgen_bb25b8_function_name" {
+  value = module.stackgen_f302a517-752b-52d7-b0d6-359ca150c8c8.name
+  sensitive = false
+}
+
+
+output "aws_lambda_stackgen_bb25b8_function_function_arn" {
+  value = module.stackgen_cbe0e684-31d0-5742-8368-e7be181bd7a5.function_arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_bb25b8_function_function_name" {
+  value = module.stackgen_cbe0e684-31d0-5742-8368-e7be181bd7a5.function_name
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_bb25b8_function_function_url" {
+  value = module.stackgen_cbe0e684-31d0-5742-8368-e7be181bd7a5.function_url
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_bb25b8_function_invoke_arn" {
+  value = module.stackgen_cbe0e684-31d0-5742-8368-e7be181bd7a5.invoke_arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_bb25b8_function_arn" {
+  value = module.stackgen_cbe0e684-31d0-5742-8368-e7be181bd7a5.arn
+  sensitive = false
+}
