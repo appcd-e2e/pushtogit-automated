438f66b8a3426342a32253d9bb590af575bf4004
stackgen-automation <automation@stackgen.com>
2025-01-28 07:48:34 +0000

    Automated commit

---
+++ terraform/README.md
@@ -0,0 +1,3 @@
+# README
+This is a readme file for IaC generated with StackGen.
+You can modify your appStack -> [here](http://main.dev.stackgen.com/appstacks/a2cbc2a5-4314-47b1-8710-390de114dcf2)
---
+++ terraform/outputs.tf
@@ -0,0 +1,10 @@
+
+output "aws_api_gateway_http_api_test_gateway_apigateway_id" {
+  value = module.stackgen_1c8ae87f-17d0-41e1-8aca-583f945ca0cc.apigateway_id
+  sensitive = false
+}
+
+output "aws_api_gateway_http_api_test_gateway_arn" {
+  value = module.stackgen_1c8ae87f-17d0-41e1-8aca-583f945ca0cc.arn
+  sensitive = false
+}
