053988d6eaf65401041e601ecc28409d15b27c24
stackgen-automation <automation@stackgen.com>
2025-04-03 16:14:38 +0000

    Automated Commit - 1743696815992

---
+++ automatedpr/terraform/outputs.tf
@@ -0,0 +1,47 @@
+
+output "aws_cloudwatch_log_group_stackgen_f713923c-90eb-56b0-be6a-3ec7ed16a104_arn" {
+  value = module.stackgen_f713923c-90eb-56b0-be6a-3ec7ed16a104.arn
+  sensitive = false
+}
+
+output "aws_cloudwatch_log_group_stackgen_f713923c-90eb-56b0-be6a-3ec7ed16a104_name" {
+  value = module.stackgen_f713923c-90eb-56b0-be6a-3ec7ed16a104.name
+  sensitive = false
+}
+
+
+output "aws_iam_role_stackgen_2d85e7a7-6dd7-5a07-b7f2-2db5965cb477_arn" {
+  value = module.stackgen_2d85e7a7-6dd7-5a07-b7f2-2db5965cb477.arn
+  sensitive = false
+}
+
+output "aws_iam_role_stackgen_2d85e7a7-6dd7-5a07-b7f2-2db5965cb477_name" {
+  value = module.stackgen_2d85e7a7-6dd7-5a07-b7f2-2db5965cb477.name
+  sensitive = false
+}
+
+
+output "aws_lambda_stackgen_e8960028-15c5-53a3-916d-518a2157609f_arn" {
+  value = module.stackgen_e8960028-15c5-53a3-916d-518a2157609f.arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_e8960028-15c5-53a3-916d-518a2157609f_function_arn" {
+  value = module.stackgen_e8960028-15c5-53a3-916d-518a2157609f.function_arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_e8960028-15c5-53a3-916d-518a2157609f_function_name" {
+  value = module.stackgen_e8960028-15c5-53a3-916d-518a2157609f.function_name
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_e8960028-15c5-53a3-916d-518a2157609f_function_url" {
+  value = module.stackgen_e8960028-15c5-53a3-916d-518a2157609f.function_url
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_e8960028-15c5-53a3-916d-518a2157609f_invoke_arn" {
+  value = module.stackgen_e8960028-15c5-53a3-916d-518a2157609f.invoke_arn
+  sensitive = false
+}
