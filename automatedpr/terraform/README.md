7be5e7af9150f503188a9f22b26522ae4dd0d452
stackgen-automation <automation@stackgen.com>
2025-04-09 07:49:50 +0000

    Automated Commit - 1744184936733

---
+++ automatedpr/terraform/README.md
@@ -0,0 +1,3 @@
+# README
+This is a readme file for IaC generated with StackGen.
+You can modify your appStack -> [here](http://main.dev.stackgen.com/appstacks/146e8064-15fc-4ae6-be45-0c24f38688e8)
---
+++ automatedpr/terraform/outputs.tf
@@ -0,0 +1,47 @@
+
+output "aws_cloudwatch_log_group_stackgen_7fbaf72d-07a9-51cc-b6a2-6e83ea54fb71_arn" {
+  value = module.stackgen_7fbaf72d-07a9-51cc-b6a2-6e83ea54fb71.arn
+  sensitive = false
+}
+
+output "aws_cloudwatch_log_group_stackgen_7fbaf72d-07a9-51cc-b6a2-6e83ea54fb71_name" {
+  value = module.stackgen_7fbaf72d-07a9-51cc-b6a2-6e83ea54fb71.name
+  sensitive = false
+}
+
+
+output "aws_iam_role_stackgen_4f2e9cec-488c-5873-8571-ee6cdfd2d140_arn" {
+  value = module.stackgen_4f2e9cec-488c-5873-8571-ee6cdfd2d140.arn
+  sensitive = false
+}
+
+output "aws_iam_role_stackgen_4f2e9cec-488c-5873-8571-ee6cdfd2d140_name" {
+  value = module.stackgen_4f2e9cec-488c-5873-8571-ee6cdfd2d140.name
+  sensitive = false
+}
+
+
+output "aws_lambda_stackgen_0daed40d-9a3d-5491-bb4d-68114af23278_arn" {
+  value = module.stackgen_0daed40d-9a3d-5491-bb4d-68114af23278.arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_0daed40d-9a3d-5491-bb4d-68114af23278_function_arn" {
+  value = module.stackgen_0daed40d-9a3d-5491-bb4d-68114af23278.function_arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_0daed40d-9a3d-5491-bb4d-68114af23278_function_name" {
+  value = module.stackgen_0daed40d-9a3d-5491-bb4d-68114af23278.function_name
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_0daed40d-9a3d-5491-bb4d-68114af23278_function_url" {
+  value = module.stackgen_0daed40d-9a3d-5491-bb4d-68114af23278.function_url
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_0daed40d-9a3d-5491-bb4d-68114af23278_invoke_arn" {
+  value = module.stackgen_0daed40d-9a3d-5491-bb4d-68114af23278.invoke_arn
+  sensitive = false
+}
