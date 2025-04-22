bed7e7f90b9194a7b143627104d1cee8ba4bf5ba
stackgen-automation <automation@stackgen.com>
2025-04-22 14:27:45 +0000

    Automated commit

---
+++ terraform/outputs.tf
@@ -0,0 +1,20 @@
+
+output "aws_s3_stackgen_f941decb-fac4-4ff4-8609-f562b04ae0b2_arn" {
+  value = module.stackgen_f941decb-fac4-4ff4-8609-f562b04ae0b2.arn
+  sensitive = false
+}
+
+output "aws_s3_stackgen_f941decb-fac4-4ff4-8609-f562b04ae0b2_bucket_name" {
+  value = module.stackgen_f941decb-fac4-4ff4-8609-f562b04ae0b2.bucket_name
+  sensitive = false
+}
+
+output "aws_s3_stackgen_f941decb-fac4-4ff4-8609-f562b04ae0b2_bucket_website_endpoint" {
+  value = module.stackgen_f941decb-fac4-4ff4-8609-f562b04ae0b2.bucket_website_endpoint
+  sensitive = false
+}
+
+output "aws_s3_stackgen_f941decb-fac4-4ff4-8609-f562b04ae0b2_kms_arn" {
+  value = module.stackgen_f941decb-fac4-4ff4-8609-f562b04ae0b2.kms_arn
+  sensitive = false
+}
