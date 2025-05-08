eb8c883b03644184153424d72bc06a783002d5b0
stackgen-automation <automation@stackgen.com>
2025-05-08 17:13:21 +0000

    Automated Commit - 1746724385166

---
+++ automatedpr/terraform/README.md
@@ -0,0 +1,3 @@
+# README
+This is a readme file for IaC generated with StackGen.
+You can modify your appStack -> [here](http://main.dev.stackgen.com/appstacks/bca6540b-9da8-4438-9956-4c7f96edadb0)
---
+++ automatedpr/terraform/outputs.tf
@@ -0,0 +1,45 @@
+output "aws_cloudwatch_log_group_stackgen_ba56d7e9-0028-572c-b441-5ee79a6094f2_arn" {
+  value     = module.stackgen_ba56d7e9-0028-572c-b441-5ee79a6094f2.arn
+  sensitive = false
+}
+
+output "aws_cloudwatch_log_group_stackgen_ba56d7e9-0028-572c-b441-5ee79a6094f2_name" {
+  value     = module.stackgen_ba56d7e9-0028-572c-b441-5ee79a6094f2.name
+  sensitive = false
+}
+
+output "aws_iam_role_stackgen_876498ec-2b62-58bd-9d9a-9c5a097b1b5d_arn" {
+  value     = module.stackgen_876498ec-2b62-58bd-9d9a-9c5a097b1b5d.arn
+  sensitive = false
+}
+
+output "aws_iam_role_stackgen_876498ec-2b62-58bd-9d9a-9c5a097b1b5d_name" {
+  value     = module.stackgen_876498ec-2b62-58bd-9d9a-9c5a097b1b5d.name
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_fca58056-dcd4-5d7f-bd69-8b5eaf2143ea_arn" {
+  value     = module.stackgen_fca58056-dcd4-5d7f-bd69-8b5eaf2143ea.arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_fca58056-dcd4-5d7f-bd69-8b5eaf2143ea_function_arn" {
+  value     = module.stackgen_fca58056-dcd4-5d7f-bd69-8b5eaf2143ea.function_arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_fca58056-dcd4-5d7f-bd69-8b5eaf2143ea_function_name" {
+  value     = module.stackgen_fca58056-dcd4-5d7f-bd69-8b5eaf2143ea.function_name
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_fca58056-dcd4-5d7f-bd69-8b5eaf2143ea_function_url" {
+  value     = module.stackgen_fca58056-dcd4-5d7f-bd69-8b5eaf2143ea.function_url
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_fca58056-dcd4-5d7f-bd69-8b5eaf2143ea_invoke_arn" {
+  value     = module.stackgen_fca58056-dcd4-5d7f-bd69-8b5eaf2143ea.invoke_arn
+  sensitive = false
+}
+
