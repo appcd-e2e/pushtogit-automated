c9e3b14b52feca0f806225095fb903a09ed19d21
stackgen-automation <automation@stackgen.com>
2025-05-08 17:17:08 +0000

    Automated Commit - 1746724610410

---
+++ terraform/README.md
@@ -0,0 +1,3 @@
+# README
+This is a readme file for IaC generated with StackGen.
+You can modify your appStack -> [here](http://main.dev.stackgen.com/appstacks/14240e67-5eae-4779-947d-3534318bf450)
---
+++ terraform/outputs.tf
@@ -0,0 +1,45 @@
+output "aws_cloudwatch_log_group_stackgen_751f3b94-0e10-51b3-8000-b9c9875e46e9_arn" {
+  value     = module.stackgen_751f3b94-0e10-51b3-8000-b9c9875e46e9.arn
+  sensitive = false
+}
+
+output "aws_cloudwatch_log_group_stackgen_751f3b94-0e10-51b3-8000-b9c9875e46e9_name" {
+  value     = module.stackgen_751f3b94-0e10-51b3-8000-b9c9875e46e9.name
+  sensitive = false
+}
+
+output "aws_iam_role_stackgen_a28ea704-2efd-5709-8e14-9dd9874c0735_arn" {
+  value     = module.stackgen_a28ea704-2efd-5709-8e14-9dd9874c0735.arn
+  sensitive = false
+}
+
+output "aws_iam_role_stackgen_a28ea704-2efd-5709-8e14-9dd9874c0735_name" {
+  value     = module.stackgen_a28ea704-2efd-5709-8e14-9dd9874c0735.name
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_000652e1-2fa3-5f1d-9331-1dab1651c295_arn" {
+  value     = module.stackgen_000652e1-2fa3-5f1d-9331-1dab1651c295.arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_000652e1-2fa3-5f1d-9331-1dab1651c295_function_arn" {
+  value     = module.stackgen_000652e1-2fa3-5f1d-9331-1dab1651c295.function_arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_000652e1-2fa3-5f1d-9331-1dab1651c295_function_name" {
+  value     = module.stackgen_000652e1-2fa3-5f1d-9331-1dab1651c295.function_name
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_000652e1-2fa3-5f1d-9331-1dab1651c295_function_url" {
+  value     = module.stackgen_000652e1-2fa3-5f1d-9331-1dab1651c295.function_url
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_000652e1-2fa3-5f1d-9331-1dab1651c295_invoke_arn" {
+  value     = module.stackgen_000652e1-2fa3-5f1d-9331-1dab1651c295.invoke_arn
+  sensitive = false
+}
+
