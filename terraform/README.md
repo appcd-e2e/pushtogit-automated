7caa67d79491a5f65267bf779b637d7c1e203809
stackgen-automation <automation@stackgen.com>
2025-05-12 16:04:47 +0000

    Automated Commit - 1747065876735

---
+++ terraform/README.md
@@ -0,0 +1,3 @@
+# README
+This is a readme file for IaC generated with StackGen.
+You can modify your appStack -> [here](http://cloud.stackgen.com/appstacks/3b7866c6-7d88-424b-82e5-e0ac8d11abcf)
---
+++ terraform/outputs.tf
@@ -0,0 +1,45 @@
+output "aws_cloudwatch_log_group_stackgen_bd3cbc4c-3109-5b7b-a13f-c62aec3b9707_arn" {
+  value     = module.stackgen_bd3cbc4c-3109-5b7b-a13f-c62aec3b9707.arn
+  sensitive = false
+}
+
+output "aws_cloudwatch_log_group_stackgen_bd3cbc4c-3109-5b7b-a13f-c62aec3b9707_name" {
+  value     = module.stackgen_bd3cbc4c-3109-5b7b-a13f-c62aec3b9707.name
+  sensitive = false
+}
+
+output "aws_iam_role_stackgen_b4cd2bf2-60d9-58b2-8dac-63faca053724_arn" {
+  value     = module.stackgen_b4cd2bf2-60d9-58b2-8dac-63faca053724.arn
+  sensitive = false
+}
+
+output "aws_iam_role_stackgen_b4cd2bf2-60d9-58b2-8dac-63faca053724_name" {
+  value     = module.stackgen_b4cd2bf2-60d9-58b2-8dac-63faca053724.name
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_b279a547-9834-59df-8581-7c0efac8c0fe_arn" {
+  value     = module.stackgen_b279a547-9834-59df-8581-7c0efac8c0fe.arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_b279a547-9834-59df-8581-7c0efac8c0fe_function_arn" {
+  value     = module.stackgen_b279a547-9834-59df-8581-7c0efac8c0fe.function_arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_b279a547-9834-59df-8581-7c0efac8c0fe_function_name" {
+  value     = module.stackgen_b279a547-9834-59df-8581-7c0efac8c0fe.function_name
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_b279a547-9834-59df-8581-7c0efac8c0fe_function_url" {
+  value     = module.stackgen_b279a547-9834-59df-8581-7c0efac8c0fe.function_url
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_b279a547-9834-59df-8581-7c0efac8c0fe_invoke_arn" {
+  value     = module.stackgen_b279a547-9834-59df-8581-7c0efac8c0fe.invoke_arn
+  sensitive = false
+}
+
