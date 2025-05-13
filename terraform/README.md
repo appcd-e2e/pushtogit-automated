52517dffc6837316b3193b9a679386b74c5b2c30
stackgen-automation <automation@stackgen.com>
2025-05-13 10:20:39 +0000

    Automated Commit - 1747131619904

---
+++ terraform/README.md
@@ -0,0 +1,3 @@
+# README
+This is a readme file for IaC generated with StackGen.
+You can modify your appStack -> [here](http://stage.dev.stackgen.com/appstacks/746efaef-4f42-41d6-b9c5-c6175c11c8a0)
---
+++ terraform/outputs.tf
@@ -0,0 +1,45 @@
+output "aws_cloudwatch_log_group_stackgen_a9586da6-e489-502a-972c-18bfdc0f0d33_arn" {
+  value     = module.stackgen_a9586da6-e489-502a-972c-18bfdc0f0d33.arn
+  sensitive = false
+}
+
+output "aws_cloudwatch_log_group_stackgen_a9586da6-e489-502a-972c-18bfdc0f0d33_name" {
+  value     = module.stackgen_a9586da6-e489-502a-972c-18bfdc0f0d33.name
+  sensitive = false
+}
+
+output "aws_iam_role_stackgen_fe4418c8-dffb-51b7-913a-927136c282ba_arn" {
+  value     = module.stackgen_fe4418c8-dffb-51b7-913a-927136c282ba.arn
+  sensitive = false
+}
+
+output "aws_iam_role_stackgen_fe4418c8-dffb-51b7-913a-927136c282ba_name" {
+  value     = module.stackgen_fe4418c8-dffb-51b7-913a-927136c282ba.name
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_3141f619-8659-5586-9b69-2fa6b7040479_arn" {
+  value     = module.stackgen_3141f619-8659-5586-9b69-2fa6b7040479.arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_3141f619-8659-5586-9b69-2fa6b7040479_function_arn" {
+  value     = module.stackgen_3141f619-8659-5586-9b69-2fa6b7040479.function_arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_3141f619-8659-5586-9b69-2fa6b7040479_function_name" {
+  value     = module.stackgen_3141f619-8659-5586-9b69-2fa6b7040479.function_name
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_3141f619-8659-5586-9b69-2fa6b7040479_function_url" {
+  value     = module.stackgen_3141f619-8659-5586-9b69-2fa6b7040479.function_url
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_3141f619-8659-5586-9b69-2fa6b7040479_invoke_arn" {
+  value     = module.stackgen_3141f619-8659-5586-9b69-2fa6b7040479.invoke_arn
+  sensitive = false
+}
+
