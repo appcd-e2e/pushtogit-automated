84d22be6d13598042f706b4f78847d35e95a8378
stackgen-automation <automation@stackgen.com>
2025-05-15 12:32:43 +0000

    Automated Commit - 1747312316977

---
+++ automatedpr/terraform/README.md
@@ -0,0 +1,3 @@
+# README
+This is a readme file for IaC generated with StackGen.
+You can modify your appStack -> [here](http://main.dev.stackgen.com/appstacks/306f2d54-751a-4989-b84b-f473828c17c5)
---
+++ automatedpr/terraform/outputs.tf
@@ -0,0 +1,45 @@
+output "aws_cloudwatch_log_group_stackgen_d5b46370-0dad-51f4-8e95-69535bf74930_arn" {
+  value     = module.stackgen_d5b46370-0dad-51f4-8e95-69535bf74930.arn
+  sensitive = false
+}
+
+output "aws_cloudwatch_log_group_stackgen_d5b46370-0dad-51f4-8e95-69535bf74930_name" {
+  value     = module.stackgen_d5b46370-0dad-51f4-8e95-69535bf74930.name
+  sensitive = false
+}
+
+output "aws_iam_role_stackgen_eea5f1ba-e841-5b3a-80de-e31835c90749_arn" {
+  value     = module.stackgen_eea5f1ba-e841-5b3a-80de-e31835c90749.arn
+  sensitive = false
+}
+
+output "aws_iam_role_stackgen_eea5f1ba-e841-5b3a-80de-e31835c90749_name" {
+  value     = module.stackgen_eea5f1ba-e841-5b3a-80de-e31835c90749.name
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_f46d0d45-8b87-5f26-8692-0d48f1cb5453_arn" {
+  value     = module.stackgen_f46d0d45-8b87-5f26-8692-0d48f1cb5453.arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_f46d0d45-8b87-5f26-8692-0d48f1cb5453_function_arn" {
+  value     = module.stackgen_f46d0d45-8b87-5f26-8692-0d48f1cb5453.function_arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_f46d0d45-8b87-5f26-8692-0d48f1cb5453_function_name" {
+  value     = module.stackgen_f46d0d45-8b87-5f26-8692-0d48f1cb5453.function_name
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_f46d0d45-8b87-5f26-8692-0d48f1cb5453_function_url" {
+  value     = module.stackgen_f46d0d45-8b87-5f26-8692-0d48f1cb5453.function_url
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_f46d0d45-8b87-5f26-8692-0d48f1cb5453_invoke_arn" {
+  value     = module.stackgen_f46d0d45-8b87-5f26-8692-0d48f1cb5453.invoke_arn
+  sensitive = false
+}
+
