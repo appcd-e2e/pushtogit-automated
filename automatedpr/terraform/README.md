3780b8707161d674bf9b020d06bcf7cb54e638af
stackgen-automation <automation@stackgen.com>
2025-05-22 09:24:04 +0000

    Automated Commit - 1747905773010

---
+++ automatedpr/terraform/README.md
@@ -0,0 +1,3 @@
+# README
+This is a readme file for IaC generated with StackGen.
+You can modify your appStack -> [here](http://main.dev.stackgen.com/appstacks/46833436-b6f5-4157-9448-071bfb6cb8f3)
---
+++ automatedpr/terraform/outputs.tf
@@ -0,0 +1,45 @@
+output "aws_cloudwatch_log_group_stackgen_cb911806-d385-5077-8faa-94bd41544e2c_arn" {
+  value     = module.stackgen_cb911806-d385-5077-8faa-94bd41544e2c.arn
+  sensitive = false
+}
+
+output "aws_cloudwatch_log_group_stackgen_cb911806-d385-5077-8faa-94bd41544e2c_name" {
+  value     = module.stackgen_cb911806-d385-5077-8faa-94bd41544e2c.name
+  sensitive = false
+}
+
+output "aws_iam_role_stackgen_061c4aab-bcbf-57dc-b16e-dcc58316345f_arn" {
+  value     = module.stackgen_061c4aab-bcbf-57dc-b16e-dcc58316345f.arn
+  sensitive = false
+}
+
+output "aws_iam_role_stackgen_061c4aab-bcbf-57dc-b16e-dcc58316345f_name" {
+  value     = module.stackgen_061c4aab-bcbf-57dc-b16e-dcc58316345f.name
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_2191b6b6-3e7d-5508-815a-bdc52cafe160_arn" {
+  value     = module.stackgen_2191b6b6-3e7d-5508-815a-bdc52cafe160.arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_2191b6b6-3e7d-5508-815a-bdc52cafe160_function_arn" {
+  value     = module.stackgen_2191b6b6-3e7d-5508-815a-bdc52cafe160.function_arn
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_2191b6b6-3e7d-5508-815a-bdc52cafe160_function_name" {
+  value     = module.stackgen_2191b6b6-3e7d-5508-815a-bdc52cafe160.function_name
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_2191b6b6-3e7d-5508-815a-bdc52cafe160_function_url" {
+  value     = module.stackgen_2191b6b6-3e7d-5508-815a-bdc52cafe160.function_url
+  sensitive = false
+}
+
+output "aws_lambda_stackgen_2191b6b6-3e7d-5508-815a-bdc52cafe160_invoke_arn" {
+  value     = module.stackgen_2191b6b6-3e7d-5508-815a-bdc52cafe160.invoke_arn
+  sensitive = false
+}
+
